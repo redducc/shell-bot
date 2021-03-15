04009ec3e11fe429cbbd551e1541dc66e32ea329
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-15 21:50:33 +0530

    Delete install-rclone.sh

--- install-rclone.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-unzip rclone-current-linux-amd64.zip
-cd rclone-*-linux-amd64
-cp rclone /usr/bin/
-chmod 755 /usr/bin/rclone
