b918a20bfb13ef1770dcd0567dc6595793a0d4e3
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-17 18:25:01 +0530

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,4 +1,4 @@
 wget -O rclone.gz https://git.io/JJMSG
 gzip -d rclone.gz
 chmod 777 rclone;mkdir /app/Shell-bot/notroot/usr;mkdir /app/Shell-bot/notroot/usr/bin 
-cp rclone /app/Shell-bot/notroot/usr/bin/ ; rm rclone
+mv rclone /app/Shell-bot/notroot/usr/bin/
