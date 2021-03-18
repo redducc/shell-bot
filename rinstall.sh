7e239871b4c18c98961983058b8977bf3bb8930e
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-18 07:27:18 +0530

    Update rinstall.sh

--- rinstall.sh
+++ rinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+curl -qO https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
 cd rclone-*-linux-amd64
 cp rclone /usr/bin/
@@ -8,4 +8,4 @@ chmod 755 /usr/bin/rclone
 wget -qO gclone.gz https://git.io/JJMSG
 gzip -d gclone.gz
 mv gclone /usr/bin/
-chmod 755 /usr/bin/rclone
+chmod 755 /usr/bin/gclone
