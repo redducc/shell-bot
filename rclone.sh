a92528ccee949d3b675ebb0e5d19472581343914
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-18 10:28:12 +0530

    Delete rclone.sh

--- rclone.sh
+++
@@ -1,4 +0,0 @@
-wget -qO rclone.gz https://git.io/JJMSG
-gzip -d rclone.gz
-chmod 777 rclone;mkdir /app/Shell-bot/notroot/usr;mkdir /app/Shell-bot/notroot/usr/bin 
-mv rclone /app/Shell-bot/notroot/usr/bin/
